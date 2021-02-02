a3dd3da21f84f79479a8d59bb1b6ccdbf7f81dd5
Dario Aceituno <darioaceituno@gmail.com>
2021-02-02 17:33:30 -0600

    Event Binding

--- my-first-app/src/app/servers/servers.component.ts
+++ my-first-app/src/app/servers/servers.component.ts
@@ -1,22 +1,27 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  //selector: '[app-servers]',
-  //selector: '.app-servers',
+  // selector: '[app-servers]',
+  // selector: '.app-servers',
   selector: 'app-servers',
   templateUrl: './servers.component.html',
 })
 export class ServersComponent implements OnInit {
 
   allowNewServer = false;
+  serverCreationStatus = 'No server was created!';
 
   constructor() {
     setTimeout( () => {
       this.allowNewServer = true;
-    },2000)
+    }, 2000);
   }
 
   ngOnInit(): void {
+  }
+
+  onCreateServer() {
+    this.serverCreationStatus = 'Server was created!';
   }
 
 }
