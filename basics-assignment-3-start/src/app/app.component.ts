cbcb0dad6bfaa8d9405927bb7866cddfc283904a
Dario Aceituno <darioaceituno@gmail.com>
2021-02-03 22:12:06 -0600

    Assignment 3 - Done

--- basics-assignment-3-start/src/app/app.component.ts
+++ basics-assignment-3-start/src/app/app.component.ts
@@ -6,4 +6,15 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+
+  displayDetails = false;
+  counter = 0;
+  arrButtonClickedCounter = [];
+
+  onDisplayDetails() {
+    this.displayDetails = !this.displayDetails;
+    this.counter += 1;
+    this.arrButtonClickedCounter.push(this.counter);
+  }
+
 }
